73f97b3549306e3ed573f2cf497c8305a15f42a5
Yifan <372104922@qq.com>
2022-11-08 16:14:12 +0800

    Add icon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,50 @@
 <template>
-  <div id="app">123</div>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe600;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe698;</div>
+    <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe7b3;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe78b;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: 1px solid #F1F1F1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+    &--active {
+      color: #1FA4FC;
+    }
+  }
+  &__title {
+    font-size: 20px;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
+}
 </style>
