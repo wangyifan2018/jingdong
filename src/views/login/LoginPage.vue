8e927690b71e1dbed2d6691fa5a6a8dd3d6455e6
Yifan <372104922@qq.com>
2022-11-14 11:03:20 +0800

    Mock 登录请求

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -2,13 +2,19 @@
   <div class="wrapper">
     <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png" />
     <div class="wrapper__input">
-      <input class="wrapper__input__content" placeholder="请输入手机号">
+      <input
+        class="wrapper__input__content"
+        placeholder="请输入用户名"
+        v-model="data.username"
+        />
     </div>
     <div class="wrapper__input">
       <input
         class="wrapper__input__content"
         placeholder="请输入密码"
-        type="password">
+        type="password"
+        v-model="data.password"
+        />
     </div>
     <div class="wrapper__login-button" @click="handleLogin">登录</div>
     <div class="wrapper__login-link" @click="handleRegisterClick">立即注册</div>
@@ -17,18 +23,33 @@
 
 <script>
 import { useRouter } from 'vue-router'
+import axios from 'axios'
+import { reactive } from '@vue/reactivity'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+
 export default {
   name: 'LoginPage',
   setup () {
+    const data = reactive({
+      username: '',
+      password: ''
+    })
     const router = useRouter()
     const handleLogin = () => {
-      localStorage.isLogin = true
-      router.push({ name: 'HomePage' })
+      axios.post('https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/api/user/login', {
+        username: data.username,
+        password: data.password
+      }).then(() => {
+        localStorage.isLogin = true
+        router.push({ name: 'HomePage' })
+      }).catch(() => {
+        alert('登录失败')
+      })
     }
     const handleRegisterClick = () => {
       router.push({ name: 'RegisterPage' })
     }
-    return { handleLogin, handleRegisterClick }
+    return { handleLogin, handleRegisterClick, data }
   }
 }
 </script>
